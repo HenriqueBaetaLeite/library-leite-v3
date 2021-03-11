287b5d264d7701152e6d3b0b46ef680fd2f43470
henrique <hbaetaleite@gmail.com>
2021-03-11 10:32:03 -0300

    versao 3 libraryleite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
+import Home from './Home';
+import Teste from './Teste';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>RAC Leite</h2>
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route path="/teste" component={Teste} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
